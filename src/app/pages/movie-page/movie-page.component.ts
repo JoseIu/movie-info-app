8898e6c895c0be33e7a6094ea70b85e427aa5601
Joselu <joseluissgz97@gmail.com>
2024-01-17 22:32:36 +0100

    movie page component

--- src/app/pages/movie-page/movie-page.component.ts
+++ src/app/pages/movie-page/movie-page.component.ts
@@ -2,14 +2,16 @@ import { CommonModule } from '@angular/common';
 import { Component, OnInit, inject } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { switchMap } from 'rxjs';
+import { RatingComponent } from '../../components/rating/rating.component';
 import { IMG_URL_500 } from '../../helpers/imageUrl';
+import { Credits } from '../../interfaces/movie-credits.interface';
 import { MovieInfo } from '../../interfaces/movie.interface';
 import { MoviesServicesService } from '../../services/movies-services.service';
 
 @Component({
   selector: 'app-movie-page',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RatingComponent],
   templateUrl: './movie-page.component.html',
   styleUrl: './movie-page.component.scss',
 })
@@ -18,14 +20,31 @@ export class MoviePageComponent implements OnInit {
   private moviesService = inject(MoviesServicesService);
 
   public movie?: MovieInfo;
+  public creditsMovie?: Credits;
   public img_500: string = IMG_URL_500;
   ngOnInit(): void {
+    this.getMovieById();
+    this.getCreditsMovie();
+  }
+
+  public getMovieById() {
     this.activateRoute.params
       .pipe(switchMap(({ id }) => this.moviesService.getMovieById(id)))
       .subscribe((movie) => {
-        console.log(movie);
+        console.log('INFO MOVIE', movie);
 
         this.movie = movie;
+        return;
+      });
+  }
+
+  public getCreditsMovie() {
+    this.activateRoute.params
+      .pipe(switchMap(({ id }) => this.moviesService.getCreditsMovie(id)))
+      .subscribe((credits) => {
+        console.log('CREDITS', credits);
+
+        this.creditsMovie = credits;
         return;
       });
   }
