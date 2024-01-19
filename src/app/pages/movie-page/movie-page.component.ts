0dcbe572a5f935662aa0be7625224b967bf69fa3
Joselu <joseluissgz97@gmail.com>
2024-01-19 22:43:25 +0100

    refactoring movie page in components

--- src/app/pages/movie-page/movie-page.component.ts
+++ src/app/pages/movie-page/movie-page.component.ts
@@ -1,17 +1,34 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit, inject } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { switchMap } from 'rxjs';
+import { map, switchMap } from 'rxjs';
+import { ActorsCardComponent } from '../../components/actors-card/actors-card.component';
+import { CategoryMoviesComponent } from '../../components/category-movies/category-movies.component';
+import { CompaniesLogoComponent } from '../../components/companies-logo/companies-logo.component';
+import { CompaniesLogosComponent } from '../../components/companies-logos/companies-logos.component';
+import { HeaderComponent } from '../../components/movie-page-components/header/header.component';
+import { MovieFooterComponent } from '../../components/movie-page-components/movie-footer/movie-footer.component';
+import { TopCastsComponent } from '../../components/movie-page-components/top-casts/top-casts.component';
 import { RatingComponent } from '../../components/rating/rating.component';
 import { IMG_URL_500 } from '../../helpers/imageUrl';
-import { Credits } from '../../interfaces/movie-credits.interface';
+import { Cast, Credits } from '../../interfaces/movie-credits.interface';
 import { MovieInfo } from '../../interfaces/movie.interface';
 import { MoviesServicesService } from '../../services/movies-services.service';
 
 @Component({
   selector: 'app-movie-page',
   standalone: true,
-  imports: [CommonModule, RatingComponent],
+  imports: [
+    CommonModule,
+    HeaderComponent,
+    RatingComponent,
+    CompaniesLogoComponent,
+    CompaniesLogosComponent,
+    MovieFooterComponent,
+    TopCastsComponent,
+    ActorsCardComponent,
+    CategoryMoviesComponent,
+  ],
   templateUrl: './movie-page.component.html',
   styleUrl: './movie-page.component.scss',
 })
@@ -20,7 +37,7 @@ export class MoviePageComponent implements OnInit {
   private moviesService = inject(MoviesServicesService);
 
   public movie?: MovieInfo;
-  public creditsMovie?: Credits;
+  public creditsMovie?: Cast[];
   public img_500: string = IMG_URL_500;
   ngOnInit(): void {
     this.getMovieById();
@@ -41,6 +58,7 @@ export class MoviePageComponent implements OnInit {
   public getCreditsMovie() {
     this.activateRoute.params
       .pipe(switchMap(({ id }) => this.moviesService.getCreditsMovie(id)))
+      .pipe(map((credits: Credits) => credits.cast))
       .subscribe((credits) => {
         console.log('CREDITS', credits);
 
