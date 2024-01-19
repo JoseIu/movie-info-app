84106294aae018a16ca54121598fa761ead7d107
Joselu <joseluissgz97@gmail.com>
2024-01-19 22:48:51 +0100

    adding IMG_URL_300 for actors img

--- src/app/helpers/imageUrl.ts
+++ src/app/helpers/imageUrl.ts
@@ -1,5 +1,4 @@
 export const IMG_URL: string = 'https://image.tmdb.org/t/p/original';
 export const IMG_URL_500: string = 'https://image.tmdb.org/t/p/w1280';
 export const IMG_URL_LOGO: string = 'https://image.tmdb.org/t/p/w300';
-
-// export  IMG_URL;
+export const IMG_URL_300: string = 'https://image.tmdb.org/t/p/w300';
