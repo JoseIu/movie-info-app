db9851e749454b40319f11e215194d128711a04e
Joselu <joseluissgz97@gmail.com>
2024-01-22 18:55:43 +0100

    importing and using category-movies, list-of-movies

--- src/app/pages/movie-page/movie-page.component.ts
+++ src/app/pages/movie-page/movie-page.component.ts
@@ -6,6 +6,7 @@ import { ActorsCardComponent } from '../../components/actors-card/actors-card.co
 import { CategoryMoviesComponent } from '../../components/category-movies/category-movies.component';
 import { CompaniesLogoComponent } from '../../components/companies-logo/companies-logo.component';
 import { CompaniesLogosComponent } from '../../components/companies-logos/companies-logos.component';
+import { ListOfMoviesComponent } from '../../components/list-of-movies/list-of-movies.component';
 import { ListOfTrailersComponent } from '../../components/list-of-trailers/list-of-trailers.component';
 import { HeaderComponent } from '../../components/movie-page-components/header/header.component';
 import { MovieFooterComponent } from '../../components/movie-page-components/movie-footer/movie-footer.component';
@@ -15,6 +16,7 @@ import { IMG_URL_500 } from '../../helpers/imageUrl';
 import { Cast, Credits } from '../../interfaces/movie-credits.interface';
 import { MovieInfo } from '../../interfaces/movie.interface';
 import { Trailer } from '../../interfaces/movie.trailer.interface';
+import { Movie } from '../../interfaces/moviesResult';
 import { MoviesServicesService } from '../../services/movies-services.service';
 
 @Component({
@@ -31,6 +33,7 @@ import { MoviesServicesService } from '../../services/movies-services.service';
     ActorsCardComponent,
     CategoryMoviesComponent,
     ListOfTrailersComponent,
+    ListOfMoviesComponent,
   ],
   templateUrl: './movie-page.component.html',
   styleUrl: './movie-page.component.scss',
@@ -42,11 +45,14 @@ export class MoviePageComponent implements OnInit {
   public movie?: MovieInfo;
   public trailersMovie?: Trailer[];
   public creditsMovie?: Cast[];
+
+  public similarMovies?: Movie[];
   public img_500: string = IMG_URL_500;
   ngOnInit(): void {
     this.getMovieById();
     this.getCreditsMovie();
     this.getTrailersMovie();
+    this.getSimilarMovies();
   }
 
   public getMovieById() {
@@ -78,6 +84,17 @@ export class MoviePageComponent implements OnInit {
       .subscribe((trailers) => {
         console.log('TRAILERS', trailers);
         this.trailersMovie = trailers;
+        return;
+      });
+  }
+
+  public getSimilarMovies() {
+    this.activateRoute.params
+      .pipe(switchMap(({ id }) => this.moviesService.getSimilarMovies(id)))
+      .subscribe((similarMovies) => {
+        console.log('SIMILAR MOVIES', similarMovies);
+
+        this.similarMovies = similarMovies;
         return;
       });
   }
