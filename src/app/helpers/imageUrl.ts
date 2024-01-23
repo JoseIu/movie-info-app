3a28886ed0a28c376b445897455bd0bf67db1848
Joselu <joseluissgz97@gmail.com>
2024-01-23 13:15:18 +0100

    prettier format

--- src/app/helpers/imageUrl.ts
+++ src/app/helpers/imageUrl.ts
@@ -1,4 +1,4 @@
 export const IMG_URL: string = 'https://image.tmdb.org/t/p/original';
 export const IMG_URL_500: string = 'https://image.tmdb.org/t/p/w1280';
-export const IMG_URL_LOGO: string = 'https://image.tmdb.org/t/p/w300';
 export const IMG_URL_300: string = 'https://image.tmdb.org/t/p/w300';
+export const IMG_URL_LOGO: string = 'https://image.tmdb.org/t/p/w300';
